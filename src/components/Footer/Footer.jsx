997c4dbd52eab6fc1fdc266c38d9941d5dfac5f3
Enrique Seguin <enseguin@gmail.com>
2026-03-29 11:59:29 -0500

    Actualización

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer id="contacto" className="footer">
       <div className="container">
-        <div>Contacto: +51 904 635 462 · Email: soporte@yipias.com</div>
+        <div>Contacto: +51 935 893 062 · Email: soporte@yipias.com</div>
         <div className="muted">© {new Date().getFullYear()} YipiAs</div>
       </div>
     </footer>
