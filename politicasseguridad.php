0999491f48771be40a1f16dbcbbbfe9ea18018c5
yipias <136390995+yipias@users.noreply.github.com>
2026-06-03 16:13:11 -0500

    Create politicasseguridad.php

---
+++ politicasseguridad.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Redirigiendo...</title>
+    <script>
+        window.location.href = "https://yipias.com/terminoscondiciones.html";
+    </script>
+</head>
+<body>
+    <p>Redirigiendo... <a href="https://yipias.com/terminoscondiciones.html">Haz clic aquí</a></p>
+</body>
+</html>
